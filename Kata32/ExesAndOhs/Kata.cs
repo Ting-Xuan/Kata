1e651b45e4af7ed013fdcccbb7ed3754e8ab8d40
Sharon Li Ting-Xuan <sharon.li@titansoft.com.sg>
2018-08-23 18:16:21 +0800

    Add Kata32

---
+++ Kata32/ExesAndOhs/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExesAndOhs
+{
+    public class Kata
+    {
+        public static bool XO(string str)
+        {
+            return (OXNum(str, 'O') == OXNum(str, 'X'));
+        }
+
+        public static int OXNum(string input, char text)
+        {
+            int rNum = 0;
+            input = input.ToUpper();
+            rNum = input.ToCharArray().Where(x => x == text).ToList().Count;
+
+            return rNum;
+        }
+
+    }
+}
---
+++ Kata32/ExesAndOhsTest/ExesAndOhsTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ExesAndOhs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ExesAndOhsTest
+{
+    [TestClass]
+    public class ExesAndOhsTest
+    {
+        [TestMethod]
+        public void OOXXTest()
+        {
+            Assert.AreEqual(true, Kata.XO("OOXX"));
+        }
+
+
+        [TestMethod]
+        public void xooxxTest()
+        {
+            Assert.AreEqual(false, Kata.XO("xooxx"));
+        }
+
+        [TestMethod]
+        public void OOXXMTest()
+        {
+            Assert.AreEqual(true, Kata.XO("OOXXM"));
+        }
+
+        [TestMethod]
+        public void zpzpzppTest()
+        {
+            Assert.AreEqual(true, Kata.XO("zpzpzpp"));
+        }
+
+        [TestMethod]
+        public void OTest()
+        {
+            Assert.AreEqual(false, Kata.XO("O"));
+        }
+
+
+
+        [TestMethod]
+        public void OaTfuxTest()
+        {
+            Assert.AreEqual(true, Kata.XO("OaTfux"));
+        }
+
+    }
+}
