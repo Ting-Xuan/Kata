ec7a9d7ed81da99e4655ad3ad7554589d9daad38
Sharon Li Ting-Xuan <sharon.li@titansoft.com.sg>
2018-09-06 10:30:43 +0800

    add kata32

---
+++ Kata33/FindTheOddInt/Kata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheOddInt
+{
+    public class Kata
+    {
+        public static int FindOndInt(int[] nums)
+        {
+            var num1 = nums.OrderBy(g=>g).Distinct().ToArray();
+
+            var oCs = num1.Select(t => new OddCount
+                {
+                    Num = t,
+                    Count = nums.Count(x => x == t)
+                })
+                .ToList();
+
+
+            return oCs.Where(x => x.Count % 2 == 1).Select(g => g.Num).FirstOrDefault();
+
+        }
+
+        public class OddCount
+        {
+            public int Num { set; get; }
+            public int Count { set; get; }
+        }
+
+        
+
+    }
+}
---
+++ Kata33/FindTheOddIntTest/FindTheOddIntTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindTheOddInt;
+namespace FindTheOddIntTest
+{
+    [TestClass]
+    public class FindTheOddIntTest
+    {
+        [TestMethod]
+        public void OnlyOneOddInterger()
+        {
+            Assert.AreEqual(5, Kata.FindOndInt(new [] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+
+        [TestMethod]
+        public void NoOddInterger()
+        {
+            Assert.AreEqual(0, Kata.FindOndInt(new[] {1, 1, 2, 2, 3, 3}));
+        }
+    }
+}
